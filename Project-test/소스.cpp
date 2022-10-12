be1dc73fa9eea3af024a071b171283ffa4470b88
kkimDev <kkheentech@gmail.com>
2022-10-13 00:18:16 +0900

    정수형 자료형과 실수형 자료형

--- Project-test/소스.cpp
+++ Project-test/소스.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
 int main() {
 
-	cout << "Hello, World" << endl;
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+	long long n_llong = LLONG_MAX;
 
-	int a;
-	a = 7;
-	int b = 3;
-	cout << "a = " << &a << ", b = " << &b << endl;
+	cout << "int는 " << sizeof n_int << "바이트이다." << endl;
+	cout << "이 바이트의 최대값은 " << n_int << " 이다." << endl;
 
+	cout << "short은 " << sizeof n_short << "바이트이다." << endl;
+	cout << "이 바이트의 최대값은 " << n_short << " 이다." << endl;
+
+	cout << "long은 " << sizeof n_long << "바이트이다." << endl;
+	cout << "이 바이트의 최대값은 " << n_long << " 이다." << endl;
+
+	cout << "long long은 " << sizeof n_llong << "바이트이다." << endl;
+	cout << "이 바이트의 최대값은 " << n_llong << " 이다." << endl;
+
+
+	unsigned int a = -1;
+	unsigned short b = -1;
+	unsigned long c = -1;
+	unsigned long long d = -1;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+
+	float e = 3.14;
+	int f = 3.14;
+
+	cout << e << " " << f << endl;
 
 	return 0;
 }
